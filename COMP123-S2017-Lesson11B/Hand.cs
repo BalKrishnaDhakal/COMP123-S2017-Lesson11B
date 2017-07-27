bdcd65ba9f8889a0fd0268ece5e26db01b0755d5
BalKrishnaDhakal <bdhakal1@my.centennialcollege.ca>
2017-07-27 13:15:49 -0400

    Created Hand Class

---
+++ COMP123-S2017-Lesson11B/Hand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/* Name: Bal Krishna Dhakal
+ * Date: July 27, 2017
+ * Description: This is the Hand Class
+ * Version: 0.1 -Created Hand Class
+ */
+namespace COMP123_S2017_Lesson11B
+{
+    public class Hand : CardList
+    {
+        protected override void _initialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
